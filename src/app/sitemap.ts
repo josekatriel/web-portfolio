a71043d94a5e48528711fcfb23358b90cfae4c7d
Jose Katr <50991616+josekatriel@users.noreply.github.com>
2025-08-06 16:11:59 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from "next";
-import { projects } from "./data/projects";
+import { projectCategories } from "./data/projects";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl =
@@ -15,7 +15,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   }));
 
   // Dynamic project routes
-  const projectRoutes = projects.flatMap((category) =>
+  const projectRoutes = projectCategories.flatMap((category) =>
     category.projects.map((project) => ({
       url: `${baseUrl}/project/${category.id}/${project.id}`,
       lastModified: new Date().toISOString().split("T")[0],
